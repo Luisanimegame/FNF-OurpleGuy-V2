e40a26777a32a70e0dc972994957f137624d0e09
Faker_wuz ★ <96131201+Luisanimegame@users.noreply.github.com>
2024-03-02 21:18:05 -0300

    Discord

--- source/MainMenuState.hx
+++ source/MainMenuState.hx
@@ -201,7 +201,7 @@ class MainMenuState extends MusicBeatState
 			FlxG.sound.music.volume += 0.5 * FlxG.elapsed;
 			if(FreeplayState.vocals != null) FreeplayState.vocals.volume += 0.5 * elapsed;
 		}
-		
+
 		var lerpVal:Float = CoolUtil.boundTo(elapsed * 7.5, 0, 1);
 		camFollowPos.setPosition(FlxMath.lerp(camFollowPos.x, camFollow.x, lerpVal), FlxMath.lerp(camFollowPos.y, camFollow.y, lerpVal));
 
