0ecf5707eff82b6f73ee747d4953bb7e2b88175f
Faker_wuz ★ <96131201+Luisanimegame@users.noreply.github.com>
2024-03-02 22:30:46 -0300

    Test

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -3891,7 +3891,7 @@ class PlayState extends MusicBeatState
 					if(FlxTransitionableState.skipNextTransIn) {
 						CustomFadeTransition.nextCamera = null;
 					}
-					MusicBeatState.switchState(new StoryMenuState());
+					MusicBeatState.switchState(new MainMenuState());
 
 					// if ()
 					if(!ClientPrefs.getGameplaySetting('practice', false) && !ClientPrefs.getGameplaySetting('botplay', false)) {
