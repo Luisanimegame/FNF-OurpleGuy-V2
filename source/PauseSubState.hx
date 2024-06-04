841090af2f70ba68193e7c0561147c79fd6bfb3f
Faker_wuz ★ <96131201+Luisanimegame@users.noreply.github.com>
2024-06-04 00:15:06 -0300

    Eu sei oq é isso!!!!!

--- source/PauseSubState.hx
+++ source/PauseSubState.hx
@@ -262,7 +262,7 @@ class PauseSubState extends MusicBeatSubstate
 
 					WeekData.loadTheFirstEnabledMod();
 					if(PlayState.isStoryMode) {
-						MusicBeatState.switchState(new StoryMenuState());
+						MusicBeatState.switchState(new MainMenuState());
 					} else {
 						MusicBeatState.switchState(new FreeplayState());
 					}
